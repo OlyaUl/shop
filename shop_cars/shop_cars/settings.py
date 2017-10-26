77919bb559fc6adb9c35bf5245b0de0dd845af32
Olya <johndoe@example.com>
2017-10-26 16:43:53 +0300

    connect postgres in settings.py

--- shop_cars/shop_cars/settings.py
+++ shop_cars/shop_cars/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'shop_cars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}'''
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cars',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
+
 
 
 # Password validation
